c7d8940158a83320a15e264486e3ad8fa490847b
Rainer Müller <rainer@websimple.be>
2014-01-02 16:21:32 +0100

    pas besoin

--- lang/partenaires_objets_fr.php
+++ lang/partenaires_objets_fr.php
@@ -8,11 +8,7 @@ $GLOBALS[$GLOBALS['idx_lang']] = array(
 	// P
 	'partenaires_objets_titre' => 'Partenaires Objets',
 
-	// C
-	'cfg_exemple' => 'Exemple',
-	'cfg_exemple_explication' => 'Explication de cet exemple',
-	'cfg_titre_parametrages' => 'Paramétrages',
-	
+	//L
 	'label_selection_partenaires_objets'=>'Sélectionnez les objets auxquelles les partenaires peuvent être associés.',
 
 	// T
